96d3be9405248f6719a5602485f564a2b8f8f91b
Koshino_Lab(Yamaguchi) <fmacro00>
2018-02-05 16:41:58 +0900

    VRでのシミュレーションに対応 プロジェクションのテストに成功

---
+++ Assets/TrackerCalibration.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerCalibration : MonoBehaviour {
+    Vector3 localPos;
+    Quaternion localRot;
+    Transform firstParent;
+
+    bool calibrating = true;
+	// Use this for initialization
+	void Start () {
+        localPos = transform.localPosition;
+        localRot = transform.localRotation;
+        firstParent = transform.parent;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(1))
+        {
+            calibrating = !calibrating;
+        }
+
+        if(calibrating)
+        {
+            transform.parent = firstParent;
+            transform.localPosition = localPos;
+            transform.localRotation = localRot;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+	}
+}
